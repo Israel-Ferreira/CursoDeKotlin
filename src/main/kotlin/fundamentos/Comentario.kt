5f2b6917c178e02c8ad55e599754d25c42e94554
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2020-10-30 14:54:09 -0300

    Começando o curso de Kotlin

---
+++ src/main/kotlin/fundamentos/Comentario.kt
@@ -0,0 +1,14 @@
+package fundamentos
+
+/**
+ * @author Israel Ferreira,
+ * @param args lista de argumentos passsados na função main
+ * @since 2020
+ * **/
+fun main(args : Array<String>) {
+    // Comentário de uma linha
+    /*
+    * Comentário
+    * de multiplas
+    * linhas */
+}
---
+++ src/main/kotlin/fundamentos/PrimeiroPrograma.kt
@@ -0,0 +1,11 @@
+package fundamentos
+
+/*
+* A função main do kotlin é o entrypoint da sua aplicação, sendo que essa
+* recebe como argumentos um array de strings contendo os argumentos.
+* Além disso, a função lembra  a função do java, só que mais enxuta.
+* */
+
+fun main(args : Array<String>){
+    println("Meu Primeiro Programa em Kotlin")
+}
