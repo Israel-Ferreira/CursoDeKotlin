34517deba9de6fee96c41070e4d052249c410ec6
Israel-Ferreira <israelfsouza@hotmail.com>
2022-08-14 22:24:49 -0300

    Atribuicao.kt

---
+++ src/main/kotlin/fundamentos/operadores/Atribuicao.kt
@@ -0,0 +1,20 @@
+package fundamentos.operadores
+
+fun main(args: Array<String>) {
+
+    /*
+    * Operador de Atribuição
+    * */
+
+    var a : Int = 2
+    var b : Int = 3
+
+    println("Valor A: $a, Valor B: $b")
+
+    b += a // b = b + a
+
+    a -= 1 // a  = a - 1
+
+    println("Valor A: $a, Valor B: $b")
+
+}
